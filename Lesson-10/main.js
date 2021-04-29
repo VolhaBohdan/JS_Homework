9d6c094cda27bd024b8392f3f470db2d8e818608
VolhaBohdan <volha.bohdan@gmail.com>
2021-04-29 22:13:18 +0300

    Homework 10 Part 3

--- Lesson-10/main.js
+++ Lesson-10/main.js
@@ -95,3 +95,71 @@ clonedObj.array.push(2);
 
 console.log(initialObj);
 console.log(clonedObj);
+
+//Задача 3
+
+function deepEqual(obj1, obj2) {
+    if (obj1 === obj2) {
+      return true;
+    } else if (typeof obj1 === 'function' && typeof obj2 === 'function'){
+        return obj1.toString() === obj2.toString();
+    } else if (((typeof obj1 === 'object' || typeof obj1 === 'function') && obj1 != null) && ((typeof obj2 === 'object' || typeof obj2 === 'function') && obj2 != null)) {
+      if (Object.keys(obj1).length !== Object.keys(obj2).length) { return false; }
+      for (var prop in obj1) {
+        if (!deepEqual(obj1[prop], obj2[prop])) {
+          return false;
+        }
+      }
+      return true;
+    } else {
+        return obj1 === obj2;
+    }
+}
+
+var initialObj = {
+    string: 'Vasya',
+    number: 30,
+    boolean: true,
+    undefined: undefined,
+    null: null,
+    array: [1, 2, 3],
+    object: {
+        string2: 'Petrov',
+        object2: {
+            array2: [{}, {}]
+        },
+        object3: {}
+    },
+    method: function() {
+        alert('Hello');
+    }
+};
+
+
+
+var newObj = {
+    string: 'Vasya',
+    number: 30,
+    boolean: true,
+    undefined: undefined,
+    null: null,
+    array: [1, 2, 3],
+    object: {
+        string2: 'Petrov',
+        object2: {
+            array2: [{}, {}]
+        },
+        object3: {}
+    },
+    method: function() {
+        alert('Hello');
+    }
+};
+
+
+
+newObj.object.object2.array2[1].name = 'Vasya';
+newObj.array.push(2);
+
+
+    deepEqual(initialObj, newObj);
