eda08b6b63133f62666379a201359ea8841a479d
VolhaBohdan <volha.bohdan@gmail.com>
2021-05-02 22:12:22 +0300

    Homework 11

---
+++ Lesson-11/main.js
@@ -0,0 +1,71 @@
+//Задание 1:
+
+
+      function filterNumbersArr(numbers) {
+          var newArr = [];
+          numbers.forEach(function(el){
+            if (el > 0) {
+                newArr[newArr.length] = el;
+            }
+          })
+          return newArr;
+      }
+
+      filterNumbersArr([-1, 0, 2, 34, -2]);
+
+//Задание 2:
+      
+      function filterNumbersArr(numbers) {
+        var positiveNumber;
+        numbers.forEach(function(el){
+          if (el > 0 && positiveNumber == null) {
+            positiveNumber = el;
+          }
+        })
+        return positiveNumber;
+    }
+
+    filterNumbersArr([-1, -78, -4567, 0, 2, 34, -2]);
+
+    //Задание 3: 
+
+    function isPalindrome (str) {
+        var strLower = str.toLowerCase();
+        var newStr = strLower.split('').reverse().join('');
+         
+        return newStr === strLower;
+    }
+    isPalindrome('шалаШ'); // true
+    isPalindrome('привет'); // false
+
+
+
+    //Задание 4
+
+    function areAnagrams (str1, str2) {
+        var str1Sorted = str1.toLowerCase().split('').sort().join('');
+        var str2Sorted = str2.toLowerCase().split('').sort().join('');
+         
+        return str1Sorted === str2Sorted;
+    }
+      areAnagrams('кот', 'отк'); // true
+      areAnagrams('кот', 'атк'); // false
+      areAnagrams('кот', 'отко'); // false
+
+    //Задание 5
+  
+
+    function divideArr(arr, size) {
+    var newArray = [];
+
+    for(var i = 0; i < arr.length; i += size) {
+        newArray.push(arr.slice(i, i+size));
+    }
+    
+    return newArray;
+  }
+
+  divideArr([1, 2, 3, 4], 2); // [[1, 2], [3, 4]]
+  divideArr([1, 2, 3, 4, 5, 6, 7, 8], 3); // [[1, 2, 3], [4, 5, 6], [7, 8]]
+
+
