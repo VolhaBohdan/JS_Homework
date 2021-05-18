4b46ad267afd040ea08bf62b37b920a283d383fd
VolhaBohdan <volha.bohdan@gmail.com>
2021-05-19 01:25:36 +0300

    Шахматное поле

---
+++ Lesson - 14/script.js
@@ -0,0 +1,65 @@
+
+document.getElementById('x').onkeyup = function (event) {
+    checkDimensionValue(event.target.value);
+}
+
+document.getElementById('y').onkeyup = function (event) {
+    checkDimensionValue(event.target.value);
+}
+
+function checkDimensionValue(value) {
+    var button = document.getElementsByTagName('button')[0];
+    if (value == '') {
+        button.disabled = true;
+    } else {
+        button.disabled = false;
+    }
+}
+
+document.getElementsByTagName('button')[0].onclick = function () {
+    var oldTable = document.getElementsByTagName('table')[0];
+    if (oldTable != null) {
+        oldTable.remove();
+    }
+
+    var x = +document.getElementById('x').value;
+    var y = +document.getElementById('y').value;
+
+    if (Number.isInteger(x) && Number.isInteger(y) && x > 1 && x <= 10 && y > 1 && y <= 10) {
+        var table = document.createElement("table");
+        for (var i = 0; i < y; i++) {
+            var tr = document.createElement('tr');
+            for (var j = 0; j < x; j++) {
+                var td = document.createElement('td');
+                if (i % 2 == j % 2) {
+                    td.style.backgroundColor = "black";
+                }
+
+                tr.appendChild(td);
+            }
+            table.appendChild(tr);
+        }
+
+        table.addEventListener('click', invertTableColors, false);
+
+        document.getElementById('container').appendChild(table);
+    } else {
+        alert('Значения X и Y должны быть в диапазоне от 1 до 10!');
+    }
+}
+
+function invertTableColors(event){
+    var table = document.getElementsByTagName('table')[0];
+    for (var i = 0; i < table.rows.length; i++) {
+        var row = table.rows[i]
+
+        for (var j = 0; j < row.cells.length; j++) {
+          var col = row.cells[j];
+          if(col.style.backgroundColor === 'black' ){
+            col.style.backgroundColor = '';
+          } else {
+              col.style.backgroundColor = 'black';
+          }
+        }  
+     }
+}
