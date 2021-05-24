52fc6274713f11a208e0bbab26a7608f43eb916c
VolhaBohdan <volha.bohdan@gmail.com>
2021-05-24 19:21:21 +0300

    Update script.js

--- HW_practice/script.js
+++ HW_practice/script.js
@@ -1,28 +1,31 @@
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     localStorage.clear();
 })
 
-document.getElementsByTagName('button')[0].onclick = function(target) {
-    var links = document.getElementsByTagName('p')[0].children;
+document.getElementsByTagName('button')[0].onclick = function (target) {
+    var rows = document.getElementsByTagName('p');
+    rows[1].addEventListener('click', clickOnSecondP, false);
 
-    for(var i = 0; i < links.length; i++){
-        links[i].style.color = 'red';
-        links[i].style.fontWeight = 'bold';
+    var links = rows[0].children;
+
+    var style = document.createElement('style');
+    style.innerHTML = '.link { color: red; font-weight: bold }';
+    document.getElementsByTagName('head')[0].appendChild(style);
+    
+    for (var i = 0; i < links.length; i++) {
+        links[i].classList.add('link');
     }
-
-    var links = document.getElementsByTagName('p')[1].addEventListener('click', clickOnSecondP, false);
 }
 
-function clickOnSecondP(event){
-    if(event.target.tagName == 'A'){
-        if(localStorage.getItem(event.target.innerText) !== null){
+function clickOnSecondP(event) {
+    event.preventDefault();
+    if (event.target.tagName == 'A') {
+        if (localStorage.getItem(event.target.innerText) !== null) {
             alert(JSON.parse(localStorage.getItem(event.target.innerText)).path);
         } else {
-            localStorage.setItem(event.target.innerText, JSON.stringify({path:event.target.href}));
+            localStorage.setItem(event.target.innerText, JSON.stringify({ path: event.target.href }));
             event.target.href = '#';
             alert('Ссылка была сохранена в Local Storage');
         }
-        event.preventDefault();
     }
 }
-
